95bdbabb9fe384b4e23b0e608aadaac573fc0d0a
VN2024 <123037426+EN2-43@users.noreply.github.com>
2024-10-17 20:05:31 -0700

    added implement to outtakesubsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/OuttakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/OuttakeSubsystem.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class OuttakeSubsystem {
+public class OuttakeSubsystem implements Subsystem {
     private DcMotor outtakeMotor;
 
     // Constants
@@ -37,5 +37,10 @@ public class OuttakeSubsystem {
         } else {
             stopOuttake();
         }
+    }
+
+    @Override
+    public void update() {
+
     }
 }
